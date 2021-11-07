bce85f33d20831d6f7f1159028373b970a977a21
gtko <gtko@users.noreply.github.com>
2021-11-07 19:11:26 +0100

    Fix sortable

--- Http/Livewire/DataList.php
+++ Http/Livewire/DataList.php
@@ -37,75 +37,75 @@ class DataList extends Component
         if(($this->direction ?? '--') !== '--') {
             $query = $repository->newQuery();
             $fields = $type->getFields();
-                //solution 1 - order simple sur un champ direct
-                if(($fields[$this->orderBy]['sortable'] ?? $type->isSortable()) === true){
-                    $query->orderBy($this->orderBy, $this->direction); //DONE
-                }else {
-                    //solution 2 - order avec une relation belongTO
-                    if($fields[$this->orderBy]['sortable']['relations'] ?? false) {
-                            $query->orderBy(function ($query) use ($fields){
-                                $sortable = $fields[$this->orderBy]['sortable'];
-                                $relations = $sortable['relations'];
-                                if(count($relations) < 2) {
-                                    /** @var \Illuminate\Database\Eloquent\Relations\BelongsTo $relation */
-                                    $relation = $relations[0];
-                                    $query->select($sortable['field'])
-                                        ->from($relation->getModel()->getTable())
-                                        ->whereColumn(
-                                            $relation->getOwnerKeyName(),
-                                            $relation->getParent()->getTable() . '.' . $relation->getForeignKeyName()
-                                        );
-                                } else {
-                                    /** @var \Illuminate\Database\Eloquent\Relations\BelongsTo $relation */
-                                    $relation = $relations[0];
-                                    $query->select($relation->getOwnerKeyName())
-                                        ->from($relation->getModel()->getTable())
-                                        ->whereColumn(
-                                            $relation->getOwnerKeyName(),
-                                            $relation->getParent()->getTable() . '.' . $relation->getForeignKeyName()
-                                        );
+            //solution 1 - order simple sur un champ direct
+            if(($fields[$this->orderBy]['sortable'] ?? $type->isSortable()) === true){
+                $query->orderBy($this->orderBy, $this->direction); //DONE
+            }else {
+                //solution 2 - order avec une relation belongTO
+                if($fields[$this->orderBy]['sortable']['relations'] ?? false) {
+                    $query->orderBy(function ($query) use ($fields){
+                        $sortable = $fields[$this->orderBy]['sortable'];
+                        $relations = $sortable['relations'];
+                        if(count($relations) < 2) {
+                            /** Relation unique a 1 niveau  */
+                            /** @var \Illuminate\Database\Eloquent\Relations\BelongsTo $relation */
+                            $relation = $relations[0];
+                            $query->select($sortable['field'])
+                                ->from($relation->getModel()->getTable())
+                                ->whereColumn(
+                                    $relation->getOwnerKeyName(),
+                                    $relation->getParent()->getTable() . '.' . $relation->getForeignKeyName()
+                                );
+                        }
+                        else
+                        {
+                            /** @var \Illuminate\Database\Eloquent\Relations\BelongsTo $relation */
+                            $relation = $relations[0];
+                            $query//->select($relation->getModel()->getTable().'.'.$relation->getOwnerKeyName())
+                            ->from($relation->getModel()->getTable())
+                                ->whereColumn(
+                                    $relation->getModel()->getTable() .'.'. $relation->getOwnerKeyName(),
+                                    $relation->getParent()->getTable() . '.' . $relation->getForeignKeyName()
+                                );
 
-                                    /** @var \Illuminate\Database\Eloquent\Relations\BelongsToMany $relation */
-                                    $relation = $relations[1];
-                                    if(is_a($relation, BelongsToMany::class)){
-                                        $query->orderBy(function ($query) use ($relation, $sortable) {
-                                        $query->select($relation->getRelatedPivotKeyName())
-                                            ->from($relation->getTable())
-                                            ->join(
-                                                $relation->getRelated()->getTable(),
-                                                $relation->getRelatedKeyName(),
-                                                $relation->getTable() . '.' . $relation->getRelatedPivotKeyName()
-                                            )
-                                            ->whereColumn(
-                                                $relation->getTable() . '.' . $relation->getForeignPivotKeyName(),
-                                                $relation->getParent()->getTable() . '.' . $relation->getParentKeyName()
-                                            );
+                            /** @var \Illuminate\Database\Eloquent\Relations\BelongsToMany $relation */
+                            $relation = $relations[1];
+                            if(is_a($relation, BelongsToMany::class)){
 
-                                        if ($sortable['pivot'] ?? false) {
-                                            $query->where($sortable['pivot'][0], $sortable['pivot'][1]);
-                                        }
-
-                                        $query->limit(1);
-                                    });
-                                    }else{
-                                        $relation = $relations[1];
-                                        $query->orderBy(function ($query) use ($relation, $sortable) {
-                                            $query->select($sortable['field'])
-                                                ->from($relation->getModel()->getTable())
-                                                ->whereColumn(
-                                                    $relation->getOwnerKeyName(),
-                                                    $relation->getParent()->getTable() . '.' . $relation->getForeignKeyName()
-                                                );
-                                        });
-                                    }
+                                $query
+                                    ->select($relation->getRelated()->getTable() .'.'.$sortable['field'])
+                                    ->join(
+                                        $relation->getTable(),
+                                        $relations[0]->getModel()->getTable() .'.'. $relations[0]->getOwnerKeyName(),
+                                        $relation->getTable() . '.' . $relation->getForeignPivotKeyName()
+                                    )
+                                    ->join(
+                                        $relation->getRelated()->getTable(),
+                                        $relation->getRelated()->getTable().'.'.$relation->getRelatedKeyName(),
+                                        $relation->getTable() . '.' . $relation->getRelatedPivotKeyName()
+                                    )->whereColumn(
+                                        $relation->getTable() . '.' . $relation->getForeignPivotKeyName(),
+                                        $relation->getParent()->getTable() . '.' . $relation->getParentKeyName()
+                                    );
+                                if ($sortable['pivot'] ?? false) {
+                                    $query->where($relation->getTable().'.'.$sortable['pivot'][0], $sortable['pivot'][1]);
                                 }
-                            }, $this->direction);
+                            }
+                            else{
+                                $relation = $relations[1];
+                                $query->select($sortable['field'])
+                                    ->join($relation->getModel()->getTable(),
+                                        $relation->getModel()->getTable().'.'.$relation->getOwnerKeyName(),
+                                        $relation->getParent()->getTable() . '.' . $relation->getForeignKeyName()
+                                    );
+                            }
+                        }
 
-                        //Solution 3 order avec une relation d'une relation d'une relation
 
-                        //Solution 4 order avec une relation belongtomany
-                    }
+                    }, $this->direction);
+
                 }
+            }
             $repository->setQuery($query);
 
         }
