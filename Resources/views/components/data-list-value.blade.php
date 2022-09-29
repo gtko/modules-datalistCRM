8b51c18077d4a5e988767adbe4719b6b5b4a0f68
gtko <gtux.prog@gmail.com>
2022-09-29 14:11:56 +0200

    ISO prod commit

--- Resources/views/components/data-list-value.blade.php
+++ Resources/views/components/data-list-value.blade.php
@@ -1,5 +1,4 @@
 <div>
-
     @if(is_array($options))
         @if($options['action'] ?? null)
 
@@ -11,33 +10,16 @@
                         $params = $options['action']['params']($item);
                     }
                 @endphp
-                @if($options['action']['confirm'] ?? false)
-                    <x-basecore::ActionConfirm>
-                        <a href="{{$options['action']['route']($params)}}"
-                           class="font-medium whitespace-nowrap {{$options['class_link'] ?? ''}}">
+                <a href="{{$options['action']['route']($params)}}"
+                   class="font-medium whitespace-nowrap {{$options['class_link'] ?? ''}}">
+                    @if($options['component'] ?? null)
+                        <x-datalistcrm::dynamics :component="$options['component']['name']"
+                                    :datas="$datas ?? []"/>
+                    @else
 
-                            @if($options['component'] ?? null)
-                                <x-datalistcrm::dynamics :component="$options['component']['name']"
-                                                         :datas="$datas ?? []"/>
-                            @else
-
-                                {!! $value ?? '' !!}
-                            @endif
-                        </a>
-                    </x-basecore::ActionConfirm>
-                @else
-                    <a href="{{$options['action']['route']($params)}}"
-                       class="font-medium whitespace-nowrap {{$options['class_link'] ?? ''}}">
-
-                        @if($options['component'] ?? null)
-                            <x-datalistcrm::dynamics :component="$options['component']['name']"
-                                                     :datas="$datas ?? []"/>
-                        @else
-
-                            {!! $value ?? '' !!}
-                        @endif
-                    </a>
-                @endif
+                        {!! $value ?? '' !!}
+                    @endif
+                </a>
             @endcan
 
             @cannot($options['action']['permission'][0] ?? null, $options['action']['permission'][1] ?? null)
@@ -52,7 +34,7 @@
         @else
             @if($options['component'] ?? null)
                 <x-datalistcrm::dynamics :component="$options['component']['name']"
-                                         :datas="$datas ?? []"/>
+                            :datas="$datas ?? []"/>
             @else
                 {!! $value ?? '' !!}
             @endif
